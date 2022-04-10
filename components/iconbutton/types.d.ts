e48b181c48beba967eee5f7e17da982891be4c04
Zeeshan <zeeshan.zahoor@toptal.com>
2022-04-10 15:05:51 +0200

    icon btn type fix

--- components/iconbutton/types.d.ts
+++ components/iconbutton/types.d.ts
@@ -1,10 +1,7 @@
 import React, { ButtonHTMLAttributes, AnchorHTMLAttributes } from 'react';
 
-import { LinkProps } from 'next/link';
-
-export interface IconButtonProps extends React.ReactHTMLElement {
+export interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   theme: 'primary' | 'secondary';
-  className?: string;
   cut?: 'right-bottom' | 'right-top' | 'left-top' | 'none'
   icon?: any
   disabled?: boolean
