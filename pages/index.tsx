01b44af2e848708624e7d6a1145b12082b01c44b
Alvaro leal <pursuance@gmail.com>
2022-05-10 09:20:58 +0200

    Refactor main map

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,123 +1,31 @@
 import React from 'react';
 
 import Head from 'next/head';
+// import Image from 'next/image';
 
-import Layer from 'components/layer';
-import Map from 'components/map';
+import MainMap from 'containers/main-map';
+
+import Button from 'components/button';
+
+// import logo from 'public/images/logo.png';
+const renderLogo = () => (
+  <a className="absolute top-4 left-4 w-12 text-white" href="/">
+    Home
+    {/* <Image alt="Polar view logo" src={logo} width={200} height={200} /> */}
+  </a>
+);
 
 const Home: React.FC = () => (
   <div>
     <Head>
       <title>Polar View</title>
     </Head>
-    <h1>Welcome to Polar View</h1>
     <div className="w-full h-screen">
-      <Map projection="antarctic">
-        <Layer
-          url=" https://geos.polarview.aq/geoserver/polarview/wms?"
-          params={{
-            format: 'image/png',
-            version: '1.1.1',
-            transparent: true,
-            layers: 'graticule3031_10x30',
-          }}
-          minZoom={0}
-          tileSize={200}
-          className="graticule-layer"
-        />
-        {/* Coastline */}
-        <Layer
-          url=" https://geos.polarview.aq/geoserver/polarview/wms?"
-          params={{
-            format: 'image/png',
-            version: '1.1.1',
-            transparent: true,
-            layers: 'polarview:coastS10',
-          }}
-          minZoom={0}
-          tileSize={200}
-        />
-        {/* SAR subset */}
-        {/* <Layer
-          url="https://geos.polarview.aq/geoserver/polarview/wms?"
-          params={{
-            format: 'image/png',
-            version: '1.1.1',
-            transparent: true,
-            layers: 'polarview:SARSUBSET',
-          }}
-          minZoom={0}
-          tileSize={710}
-        /> */}
-
-        {/* Ice layers */}
-
-        {/* Ice Chart - MetNorway */}
-        {/* <Layer
-          url="https://geos.polarview.aq/geoserver/polarview/wms?"
-          params={{
-            format: 'image/png',
-            version: '1.1.1',
-            transparent: true,
-            layers: 'iceChartWithLandMask',
-          }}
-          minZoom={0}
-          tileSize={256}
-        /> */}
-        {/* Ice Charts - NIC */}
-
-        {/* Marginal Ice Zone (MIZ)  */}
-        <Layer
-          url="https://geos.polarview.aq/geoserver/polarview/wms?"
-          params={{
-            format: 'image/png',
-            version: '1.1.1',
-            transparent: true,
-            layers: 'polarview:antarctic_icechart_nic',
-          }}
-          minZoom={0}
-          tileSize={256}
-        />
-
-        {/* <Layer
-          url="https://polarcode.data.bas.ac.uk/geoserver/wms?"
-          params={{
-            format: 'image/png',
-            version: '1.1.1',
-            transparent: true,
-            layers: 'nrt_antarc220401',
-            styles: '01 - Ice Concentration',
-            opacity: 0.5,
-          }}
-          minZoom={0}
-          tileSize={256}
-        /> */}
-        {/* <Layer
-          url="https://geos.polarview.aq/geoserver/polarview/wms?"
-          params={{
-            format: 'image/png',
-            version: '1.1.1',
-            transparent: true,
-            layers: 'nrt_antarc220401',
-            styles: '02 - Ice Stage Of Development',
-          }}
-          minZoom={0}
-          tileSize={256}
-        /> */}
-
-        {/* Example for WMST
-        <Layer
-          url="https://geos.polarview.aq/geoserver/wms?TIME=2022-03-09T00%3A00%3A00.000Z"
-          params={{
-            format: 'image/png',
-            version: '1.1.1',
-            transparent: true,
-            layers: 's3_mosaic_s',
-          }}
-          minZoom={0}
-          tileSize={710}
-        /> */}
-      </Map>
+      {renderLogo()}
+      <div className="absolute top-4 right-4 z-50">
+        <Button theme="primary">Hello</Button>
+      </div>
+      <MainMap />
     </div>
   </div>
 );
