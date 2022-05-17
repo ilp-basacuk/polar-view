ded7c15eb96cc79953abcdd73a73ceb2e821eb2e
Alvaro leal <pursuance@gmail.com>
2022-05-17 18:07:44 +0200

    Update preact config

--- packages/low-bandwith/preact.config.js
+++ packages/low-bandwith/preact.config.js
@@ -1,7 +1,19 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const webpack  = require('webpack');
 const tailwind = require("preact-cli-tailwind");
 
 module.exports = (config, env, helpers) => {
   config = tailwind(config, env, helpers);
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    'react': 'preact-compat',
+    'react-dom': 'preact-compat'
+  };
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      Component: ['preact', 'Component'],
+      React: ['preact-compat']
+    })
+  );
   return config;
 };
