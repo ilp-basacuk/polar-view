796ef24e8075a35ec48be08a3fa0be7a760d54ae
Alvaro leal <pursuance@gmail.com>
2022-05-18 17:23:39 +0200

    Add env variables for low

--- packages/low-bandwith/preact.config.js
+++ packages/low-bandwith/preact.config.js
@@ -1,9 +1,22 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const webpack  = require('webpack');
 const tailwind = require("preact-cli-tailwind");
+const { parsed } = require('dotenv').config();
 
 module.exports = (config, env, helpers) => {
   config = tailwind(config, env, helpers);
+
+  // Environment variables
+  const { plugin } = helpers.getPluginsByName(config, 'DefinePlugin')[0];
+  Object.keys(parsed).forEach(
+    (key) => {
+    plugin.definitions[`process.env.${key}`] = JSON.stringify(parsed[key]);
+  });
+
+  // Needed for .env https://github.com/preactjs/preact-cli/issues/244#issuecomment-316558813
+  config.node.process = 'mock';
+
+  // Compatibility with react
   config.resolve.alias = {
     ...config.resolve.alias,
     'react': 'preact-compat',
@@ -15,5 +28,6 @@ module.exports = (config, env, helpers) => {
       React: ['preact-compat']
     })
   );
+
   return config;
 };
