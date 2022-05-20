1bcd9bd03d7a5da8c28338134c994545822427d8
Alvaro leal <pursuance@gmail.com>
2022-05-20 19:44:42 +0200

    Fix retain dropdowns selection

--- packages/high-bandwith/components/sidebar/groups/grouped-dropdowns.tsx
+++ packages/high-bandwith/components/sidebar/groups/grouped-dropdowns.tsx
@@ -2,6 +2,7 @@ import { FC, useState, useMemo, useEffect } from 'react';
 import Select from 'components/forms/select';
 import { updateLayer } from 'store/features/layerGroups/slice';
 import { useAppDispatch } from 'store/hooks';
+import useChangeEffect from 'components/hooks/useChangeState';
 
 interface GroupedDropdownsProps {
   layer: any,
@@ -13,7 +14,7 @@ const GroupedDropdowns: FC<GroupedDropdownsProps> = ({ layer }) => {
   const groupOptions = layer.groups.map(group => ({ label: group.label, value: group.id }));
   const layerOptions = useMemo(() => layer.layers.filter(l => l.group === selectedGroup).map(l => ({ label: l.label, value: l.id })), [selectedGroup]);
 
-  useEffect(() => {
+  useChangeEffect(() => {
     setSelectedLayer(null);
   }, [selectedGroup]);
 
