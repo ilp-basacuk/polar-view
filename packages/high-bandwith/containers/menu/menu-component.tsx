65938bb8a90485db8591cc7fbfda3d4a071c2d91
Alvaro leal <pursuance@gmail.com>
2022-05-24 19:45:33 +0200

    Add links to low in menu

--- packages/high-bandwith/containers/menu/menu-component.tsx
+++ packages/high-bandwith/containers/menu/menu-component.tsx
@@ -1,11 +1,13 @@
 import Button from 'components/button';
 
+const LOW_BANDWITH_URL = process.env.NEXT_PUBLIC_LOW_BANDWITH_URL;
+
 const Menu = () => (
   <div className="flex z-50">
-    <Button className="border-r-0 mr-0.5 text-tiny px-4" theme="primary" cut="left-top" href="/">
+    <Button className="border-r-0 mr-0.5 text-tiny px-4" theme="primary" cut="left-top" href={LOW_BANDWITH_URL}>
       HOMEPAGE
     </Button>
-    <Button className="border-l-0 px-4" theme="primary" cut="right-bottom" href="/about">
+    <Button className="border-l-0 px-4" theme="primary" cut="right-bottom" href={`${LOW_BANDWITH_URL}/about`}>
       ABOUT
     </Button>
   </div>
