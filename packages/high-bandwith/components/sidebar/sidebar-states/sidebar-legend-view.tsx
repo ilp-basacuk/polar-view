f01509da9c50a7e9214dd4785b77361faef903d3
Alvaro leal <pursuance@gmail.com>
2022-06-08 18:29:16 +0200

    Fix screen scroll and add layer label and dots

--- packages/high-bandwith/components/sidebar/sidebar-states/sidebar-legend-view.tsx
+++ packages/high-bandwith/components/sidebar/sidebar-states/sidebar-legend-view.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import cx from 'classnames';
 import type { SideBarAction } from '../types';
 import { useAppSelector } from 'store/hooks';
 import Legend from 'components/legend';
@@ -40,7 +41,19 @@ const SidebarLegendView: FC<SideBarLegendViewProps> = () => {
           <Divider label={layerGroupId} />
           {layerGroup[layerGroupId].map((layer: Layer) => (
             <div key={`legend-item-${layerGroupId}`}>
-              <Legend layer={layer} />
+              <div className="text-mainblue">
+                <span className={cx(
+                  'w-2 h-2 mr-2 ml-1 inline-block rounded-full border',
+                  {
+                    [`border-transparent bg-${layer.color}`]:  layer.color,
+                    [`border-mainblue bg-transparent`]:  !layer.color
+                  }
+                )}/>
+                {layer.label}
+              </div>
+              <div className="ml-4 my-1">
+                <Legend layer={layer} />
+              </div>
             </div>
            ))}
         </div>
