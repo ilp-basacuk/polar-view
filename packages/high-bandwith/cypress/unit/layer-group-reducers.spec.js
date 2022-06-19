e5e45f676f33b50b6c6ca4c14891bd9e75239ac5
Alvaro leal <pursuance@gmail.com>
2022-06-19 21:17:57 +0200

    Example reducer test

---
+++ packages/high-bandwith/cypress/unit/layer-group-reducers.spec.js
@@ -0,0 +1,48 @@
+import { updateLayerReducer } from  'store/features/layerGroups/slice';
+
+const layersConfig = [
+  {
+    "label": "First Group", "id": "first-group", "selected": false,
+    "layers": [
+      { "id": "layer-1", "label": "Example Layer 1", "checked": false,  "type": "checkbox", "downloadURL": "https://www.exampledownloadurl.com/1" },
+    ]
+  }
+];
+
+
+describe('Update layer reducer', () => {
+  const initialState = {
+    data: layersConfig,
+    activePreset: null,
+  };
+
+  const updatedLayer = { "id": "layer-1", "label": "Example Layer 1", "checked": true,  "type": "checkbox", "downloadURL": "https://www.exampledownloadurl.com/1" };
+  const updatedLayerData = [
+    {
+      ...layersConfig[0],
+      "layers": [
+        updatedLayer,
+      ]
+    }
+  ];
+
+  const updatedState = {
+    data: updatedLayerData,
+    activePreset: null
+  };
+  const action = { payload:  { layerGroupId: 'first-group', layer: updatedLayer } };
+
+  it('should update the layer', () => {
+    expect(updateLayerReducer(initialState, action)).to.deep.equal(updatedState);
+  });
+
+  it('should clear active preset when we update the layer', () => {
+    const initialStateWithActivePreset = {
+      data: layersConfig,
+      activePreset: 'some-active-preset',
+    };
+
+    expect(updateLayerReducer(initialStateWithActivePreset, action)).to.deep.equal(updatedState);
+  });
+
+});
